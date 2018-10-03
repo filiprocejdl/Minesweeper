a34a54ffe6167aee11a4d590883b7faa717489a1
rocejfi15 <rocejfi15@DST20717>
2018-10-03 09:40:06 +0200

    Generator

--- Miny/MainWindow.xaml.cs
+++ Miny/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Miny
 {
@@ -20,9 +22,82 @@ namespace Miny
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer dt = new DispatcherTimer();
+        Stopwatch sw = new Stopwatch();
+        string currentTime = string.Empty;
+        Boolean clicked = false;
+        int row = 0;
+        int col = 0;
         public MainWindow()
         {
             InitializeComponent();
+            Grid Field = new Grid();
+            
+            dt.Tick += new EventHandler(Time);
+            dt.Interval = new TimeSpan(0, 0, 0, 0, 1);
         }
+
+        //STOPWATCH
+        void Time(object sender, EventArgs e)
+        {
+            if (sw.IsRunning)
+            {
+                TimeSpan ts = sw.Elapsed;
+                currentTime = String.Format("{0:00}:{1:00}",
+                ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+                Timer.Content = currentTime;
+            }
+        }
+
+        private void Start_button(object sender, RoutedEventArgs e)
+        {
+            if (clicked == false)
+            {
+                sw.Start();
+                dt.Start();
+                clicked = true;
+                btn.Content = "STOP";
+            } else
+            {
+                sw.Reset();
+                Timer.Content = "00:00";
+                clicked = false;
+                btn.Content = "START";
+            }
+            
+
+        }
+
+
+        //FIELD GENERATOR
+        public void Generator(Grid Field)
+        {
+            Field.Width = 250;
+            Field.Height = 250;
+            int x = 10;
+            int y = 10;
+
+            while (row < 10)
+            {
+                RowDefinition Row = new RowDefinition();
+                Field.RowDefinitions.Add(Row);
+                row = row++;
+            }
+
+            while (col < 10)
+            {
+                ColumnDefinition Col = new ColumnDefinition();
+                Field.ColumnDefinitions.Add(Col);
+                col = col++;
+            }
+        }
+
+
+     
+
+
+       
+
+
     }
 }
